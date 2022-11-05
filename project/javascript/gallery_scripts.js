39ceb2b7db9e7496bf3b47caaf10eed9cc4f3cac
logger421 <wojteklotko3@gmail.com>
2022-11-05 23:06:36 +0100

    Upgraded code for gallery site; added few upgrades in styles file;

---
+++ project/javascript/gallery_scripts.js
@@ -0,0 +1,12 @@
+var fullImgBox = document.getElementById("fullImgBox")
+var fullImg = document.getElementById("fullImg")
+
+function openFullImg(image){
+        fullImgBox.style.display = "flex";
+        fullImg.src = image;
+}
+
+
+function closeFullImg() {
+    fullImgBox.style.display = "none"
+}
